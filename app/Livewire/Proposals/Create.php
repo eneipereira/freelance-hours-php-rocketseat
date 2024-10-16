ff0c92575ccf488242f2b9af33fb8a71cdabf79c
eneipereira <eneipereira@outlook.com.br>
2024-10-15 21:07:28 -0300

    feat: adiciona funcionalidade de atualizar as posições das propostas

--- app/Livewire/Proposals/Create.php
+++ app/Livewire/Proposals/Create.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire\Proposals;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
+use App\Models\Proposal;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
@@ -10,9 +13,9 @@ class Create extends Component
 {
     public Project $project;
 
-    
+
     public bool $modal = false;
-    
+
     #[Validate(['required', 'email'])]
     public string $email = '';
 
@@ -21,21 +24,44 @@ class Create extends Component
 
     public bool $agree = false;
 
+    public function arrangePositions($proposal)
+    {
+
+        $query = DB::select("
+        select *, row_number() over (order by hours asc) as newPosition
+        from proposals
+        where project_id = :project", ['project' => $proposal->project_id]);
+
+        $position = collect($query)->where('id', '=', $proposal->id)->first();
+        $otherProposal = collect($query)->where('position', '=', $position->newPosition)->first();
+
+        if ($otherProposal) {
+            $proposal->update(['position_status' => 'up']);
+            Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
+        }
+
+        ArrangePositions::run($proposal->project_id);
+    }
+
     public function save()
     {
-        
+
         $this->validate();
-        
-        if(!$this->agree) {
+
+        if (!$this->agree) {
             $this->addError('agree', 'Você precisa concordar com os termos.');
             return;
         }
 
-        $this->project->proposals()
-            ->updateOrCreate(
-                ['email' => $this->email],
-                ['hours' => $this->hours]
-            );
+        DB::transaction(function () {
+            $proposal = $this->project->proposals()
+                ->updateOrCreate(
+                    ['email' => $this->email],
+                    ['hours' => $this->hours]
+                );
+
+            $this->arrangePositions($proposal);
+        });
 
         $this->modal = false;
 
